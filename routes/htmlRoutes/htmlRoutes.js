70d2ca2654a9198bf72d8c2948dc72da6dbb6708
mmilner29 <madisonm4514@gmail.com>
2022-04-09 18:30:03 -0700

    htmlRoutes for each html file

--- routes/htmlRoutes/htmlRoutes.js
+++ routes/htmlRoutes/htmlRoutes.js
@@ -1,4 +1,17 @@
 const router = require('express').Router();
 const path = require('path');
 
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
+
+//wildcard
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+  });
+
 module.exports = router;
