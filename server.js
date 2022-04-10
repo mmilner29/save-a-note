951df906aa93120500a0cb6399f0d09e4f420451
mmilner29 <madisonm4514@gmail.com>
2022-04-09 18:14:40 -0700

    finish file structure

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+const PORT = process.env.PORT || 3001;
+//instantiate the server
+const app = express();
+
+//parse incoming string or arry data
+app.use(express.urlencoded({ extended: true }));
+//parse incoming JSON data
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// set up listener for PORT
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
+});
