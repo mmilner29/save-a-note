a86035ba99fa34d52e8fa8bb3f1564287bea8f2c
mmilner29 <madisonm4514@gmail.com>
2022-04-09 19:14:30 -0700

    got routes to work with server

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
+const myNotes = require('../../db/db.json');
+const fs = require('fs');
 
-
+router.get('/notes', (req,res) => {
+    res.json(myNotes.slice(1));
+});
 
 module.exports = router;
--- routes/htmlRoutes/htmlRoutes.js
+++ routes/htmlRoutes/htmlRoutes.js
@@ -1,11 +1,12 @@
-const router = require('express').Router();
 const path = require('path');
+const router = require('express').Router();
+
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
-router.get('/', (req, res) => {
+router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
 
